9f6db9bccf6a726327d4728045e537d8487ce424
Kale Franz <kalefranz@gmail.com>
2022-02-18 02:23:27 -0600

    indentation

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,18 +13,28 @@ const posts = {};
 
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
-    posts[id] = { id, title };
-    res.status(201).send(posts[id]);
+app.post('/posts', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: { id, title },
+  });
+
+  res.status(201).send(posts[id]);
 });
 
 
 app.listen(4000, () => {
-    console.log('listening on 4000');
+  console.log('listening on 4000');
 });
