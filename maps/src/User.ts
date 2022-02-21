240d7a21bcffb6466e67855d0c240afc67056da4
Kale Franz <kalefranz@gmail.com>
2022-02-20 23:17:10 -0600

    maps app for typescript section

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,3 +1,22 @@
-class User {
-  
-};
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+
+export class User implements Mappable {
+  name: string;
+  location: { 
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  };
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
+}
