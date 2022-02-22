a9dd30ee9724cbeae37262714db1ddc276405756
Kale Franz <kalefranz@gmail.com>
2022-02-22 00:46:59 -0600

    there is no set-cookie; I'm tired of trying

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-// import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
+router.get('/api/users/currentuser', (req, res) => {  // currentUser, 
   res.send({
-    currentUser: 15,  // req.currentUser || null
+    currentUser: req.currentUser || null
+    // currentUser: 15,  // req.currentUser || null
   });
 });
 
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -2,18 +2,49 @@ import axios from 'axios';
 
 
 const LandingPage = ({ currentUser }) => {
-  console.log('I am in the component', color);
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
 
-LandingPage.getInitialProps = async () => {
-  // const response = await axios.get('/api/users/currentuser');
-  // return response.data;
+// http://NAME_OF_SERVICE.NAMESPACE.svc.cluster.local
+// http://ingress-nginx-controller.ingress-nginx.svc.cluster.local   // pass along cookie as well!
 
-  console.log('I am on the server!');
-  return { color: 'red' };
-}
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    console.log('getInitialProps SERVER req:', req && req.headers );
+    // we are on the server!
+    // requests should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/{path}
+    const { data } = await axios.get(
+      // 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,  // { Host: 'example.com' },
+      }
+    );
+    return data;
+
+    
+  } else {
+    console.log('getInitialProps BROWSER req:', req && req.headers );
+
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    // const response = await axios.get('/api/users/currentuser');
+    // { currentUser: { id: 123, email: ak@aka.ala} }  // signed-in
+    // { currentUser: null }  // signed-out
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+
+  // const response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser');
+  // // .catch(err => {
+  // //   console.log(err.message);
+  // // });
+  // return response.data;
+};
 
 
 export default LandingPage;
