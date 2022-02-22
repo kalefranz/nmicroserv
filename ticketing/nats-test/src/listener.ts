f9db69d91e579ba64981686a80ffd2b7a58ab9e5
Kale Franz <kalefranz@gmail.com>
2022-02-22 06:35:25 -0600

    starting section 310

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message, Stan } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import nats from 'node-nats-streaming';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 console.clear();
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
@@ -7,29 +8,15 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 });
 
 stan.on('connect', () => {
-  console.log('Listener connected to NATS');
+  console.log('listener connected to NATS');
 
   stan.on('close', () => {
     console.log('NATS connection closed');
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true);
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'orders-service-queue-group',
-    options,
-  );
+  new TicketCreatedListener(stan).listen();
 
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
 })
 
 process.on('SIGINT', () => {
@@ -40,68 +27,3 @@ process.on('SIGTERM', () => {
   console.log('received SIGTERM');
   stan.close();
 });
-
-
-abstract class Listener {
-  abstract subject: string;
-  abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-
-  private client: Stan;
-  protected ackWait = 5 * 1000;
-
-  constructor(client: Stan) {
-    this.client = client;
-
-  }
-
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
-
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions(),
-    );
-    subscription.on('message', (msg: Message) => {
-      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
-
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  };
-
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === 'string'
-      ? JSON.parse(data)
-      : JSON.parse(data.toString('utf8'));
-  }
-  
-}
-
-
-class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
-  queueGroupName = 'payments-service';
-
-  onMessage(data: any, msg: Message): void {
-    console.log('Event data', data);
-    msg.ack();
-  }
-}
-
-
-// const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
-//   url: 'http://localhost:4222',
-// });
-// const listener = new TicketCreatedListener(nats.connect('ticketing', randomBytes(4).toString('hex'), {
-//   url: 'http://localhost:4222',
-// }));
