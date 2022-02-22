b77f17f67a4a6de3d18561e80c7bc60c902c6493
Kale Franz <kalefranz@gmail.com>
2022-02-21 22:55:07 -0600

    starting section 226

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+// import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('A Ok');
+  res.send({
+    currentUser: 15,  // req.currentUser || null
+  });
 });
 
 export { router as currentUserRouter };
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,5 +1,19 @@
-const Landing = () => {
+import axios from 'axios';
+
+
+const LandingPage = ({ currentUser }) => {
+  console.log('I am in the component', color);
   return <h1>Landing Page</h1>;
 };
 
-export default Landing;
+
+LandingPage.getInitialProps = async () => {
+  // const response = await axios.get('/api/users/currentuser');
+  // return response.data;
+
+  console.log('I am on the server!');
+  return { color: 'red' };
+}
+
+
+export default LandingPage;
